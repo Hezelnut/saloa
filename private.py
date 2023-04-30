014726bb3d02d30a1c001e2abde131b7e41053d7
Hezelnut <124646487+Hezelnut@users.noreply.github.com>
2023-04-30 14:35:08 +0900

    Update private.py

--- private.py
+++ private.py
@@ -80,7 +80,7 @@ else:pass
 
         
 st.write('Data load : ',database()[0].strftime('%m.%d - %H:%M:%S'))
-minute = divmod(time_gap.total_seconds())[0]
+minute = divmod(time_gap.total_seconds(),60)[0]
 st.write('{}분 전에 최신화되었습니다.'.format(int(minute)))
 
 
