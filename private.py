6a810534e272cab4c7ab7a48cf92dd1ad6aac0ce
Hezelnut <124646487+Hezelnut@users.noreply.github.com>
2023-05-02 14:48:37 +0900

    Update private.py

--- private.py
+++ private.py
@@ -70,14 +70,14 @@ def database():
 
 st.title("Saloa")
 
-async def reset():
-    reset_1 = st.button('데이터 최신화')
+
+reset_1 = st.button('데이터 최신화')
+async def reset():    
     if reset_1 :
         await st.cache_data.clear()
         asyncio.sleep(3600)
         await st.cache_data.clear()
     else:pass
-
 reset()
 
 time_gap = datetime.datetime.now(pytz.timezone('Asia/Seoul')) - database()[0]
